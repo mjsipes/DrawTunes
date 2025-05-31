7c3999332714b901ead91b95dd7fc36885d9b307
Michael Sipes <msipes@usc.edu>
2025-05-30 22:13:21 -0700

    really nice testing situation

--- supabase/functions/handle-upload/test.ts
+++ supabase/functions/handle-upload/test.ts
@@ -1,12 +1,24 @@
-// import "https://deno.land/std@0.220.1/dotenv/load.ts";
-// import { generateMusicRecommendations } from "./openai.ts";
-// console.log("hello from handle-upload function");
-// const result = await generateMusicRecommendations(
-//     "https://efaxdvjankrzmrmhbpxr.supabase.co/storage/v1/object/public/drawings/drawings/drawing-1747250359245.png",
-// );
-// console.log(result);
-
+import { load } from "https://deno.land/std@0.220.1/dotenv/mod.ts";
+import { generateMusicRecommendations } from "./openai.ts";
 import { searchTrack } from "./itunes.ts";
-console.log("hi from test.ts");
-const results = await searchTrack("Shape of You", "Ed Sheeran");
-console.log(results);
+
+await load({
+    envPath: "../.env",
+    export: true,
+});
+async function test1() {
+    console.log("hello from handle-upload function");
+    const result = await generateMusicRecommendations(
+        "https://efaxdvjankrzmrmhbpxr.supabase.co/storage/v1/object/public/drawings/drawings/drawing-1747250359245.png",
+    );
+    console.log(result);
+}
+
+async function test2() {
+    console.log("hi from test.ts");
+    const results = await searchTrack("Shape of You", "Ed Sheeran");
+    console.log(results);
+}
+
+test1();
+test2();
